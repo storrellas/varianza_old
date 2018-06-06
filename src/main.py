a25045a5cdbc9b2875a14808b8b0fc321fb15830
storrellas <storrellas@gmail.com!>
2018-06-06 22:29:32 +0200

    ENH: Added logging

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
+import logging
+
+
+def format_workbook():
+    #print("Formatting workbook")
+    logging.info('Format workbook')
 
 if __name__ == "__main__":
-    print("My first step")
+    FORMAT = "%(asctime)-7s | %(levelname)s  | %(message)s"
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG, datefmt='%H:%M:%S')
+    #logging.debug('Debugging message')
+    logging.info('My first step')
+    #logging.warning('And this, too')
+    format_workbook()

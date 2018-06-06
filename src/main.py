d5fc5ccb2a6c617b0367f6d70434b88678040478
storrellas <storrellas@gmail.com!>
2018-06-06 23:30:44 +0200

    WIP: Generating expected result

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import logging
 import openpyxl
+from openpyxl.styles import colors
+from openpyxl import Workbook
+from openpyxl.styles import Font, Color, Alignment
 
 
 def format_workbook(adjust_content=True, font=None, header=1,\
@@ -14,8 +17,36 @@ def format_workbook(adjust_content=True, font=None, header=1,\
     logging.info('thousand_separator %r', thousand_separator)
     wb = openpyxl.load_workbook(filename='./res/input.xlsx')
 
+
     # Print current sheetnames
     print (wb.sheetnames)
+
+    # Worksheet
+    ws = wb.active
+
+    # 1. Adjust content
+    for col in ws.columns:
+         max_length = 0
+         column = col[0].column # Get the column name
+         for cell in col:
+             try: # Necessary to avoid error on empty cells
+                 if len(str(cell.value)) > max_length:
+                     max_length = len(cell.value)
+             except:
+                 pass
+         #adjusted_width = (max_length + 2) * 1.2
+         adjusted_width = (max_length + 2)
+         ws.column_dimensions[column].width = adjusted_width
+    # for row in ws.iter_rows():
+    #     for cell in row:
+    #         cell.style = Alignment(wrapText=True)
+
+
+    # Change background<
+    for row in ws.iter_rows():
+        for cell in row:
+            cell.font = Font(name='Ubuntu')
+            #cell.font = Font(name='Ubuntu', color=colors.RED)
 
     # Save output to new filename
     wb.save(filename='./res/input_generated.xlsx')

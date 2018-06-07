56d749e7854fcca55c4c0a003ea6a7a87e1a98e4
storrellas <storrellas@gmail.com>
2018-06-07 09:03:09 +0000

    ENH: Using parameters within function

--- src/main.py
+++ src/main.py
@@ -5,8 +5,7 @@ from openpyxl import Workbook
 from openpyxl.styles import Font, Color, Alignment, PatternFill
 
 
-
-def format_workbook(adjust_content=True, font=None, header=1, header_bg=None,\
+def format_workbook(adjust_content=True, font=None, header=1, header_bg=None,
                     odd_bg=None, even_bg=None, thousand_separator=True):
     # print("Formatting workbook")
     logging.info('Format workbook')
@@ -19,20 +18,20 @@ def format_workbook(adjust_content=True, font=None, header=1, header_bg=None,\
     logging.info('thousand_separator : %r', thousand_separator)
     wb = openpyxl.load_workbook(filename='./res/input.xlsx')
 
-    # Worksheet
+    # Select active Worksheet
     ws = wb.active
 
     # 1. Change cell font
-    for row in ws.iter_rows():
-        for cell in row:
-            cell.font = Font(name='Verdana')
-            # cell.font = Font(name='Ubuntu', color=colors.RED)
+    if font is not None:
+        for row in ws.iter_rows():
+            for cell in row:
+                cell.font = Font(name=font)
 
     # 1. Adjust content
+    # See https://stackoverflow.com/questions/13197574/openpyxl-adjust-column-width-size/14450572
     for col in ws.columns:
         max_length = 0
         column = col[0].column  # Get the column name
-
 
         for cell in col:
             try:  # Necessary to avoid error on empty cells
@@ -40,19 +39,8 @@ def format_workbook(adjust_content=True, font=None, header=1, header_bg=None,\
                     max_length = len(cell.value)
             except Exception:
                 pass
-
-        print('Evaluating', column, max_length)
-
         adjusted_width = (max_length + 2) * 2
         ws.column_dimensions[column].width = adjusted_width
-    # See https://stackoverflow.com/questions/13197574/openpyxl-adjust-column-width-size/14450572
-
-
-
-    # Select backgrounds - Cheating
-    odd_bg = 'FF0000'
-    even_bg = '00F700'
-    header_bg = '0000FF'
 
     # 3. Update cell background (header, odd,even)
     for row in ws.iter_rows():
@@ -67,12 +55,14 @@ def format_workbook(adjust_content=True, font=None, header=1, header_bg=None,\
             else:
                 bg = odd_bg  # Odd
         for cell in row:
-            cell.fill = PatternFill(fgColor=bg, fill_type="solid")
+            if bg is not None:
+                cell.fill = PatternFill(fgColor=bg, fill_type="solid")
 
     # 4. Add thousands separator
-    for row in ws.iter_rows():
-        for cell in row:
-            cell.number_format = u'#,##0.00'
+    if thousand_separator:
+        for row in ws.iter_rows():
+            for cell in row:
+                cell.number_format = u'#,##0.00'
 
     # Save output to new filename
     wb.save(filename='./res/input_generated.xlsx')
@@ -84,7 +74,7 @@ def format_workbook(adjust_content=True, font=None, header=1, header_bg=None,\
 if __name__ == "__main__":
     FORMAT = "%(asctime)-7s | %(levelname)s  | %(message)s"
     logging.basicConfig(format=FORMAT, level=logging.DEBUG, datefmt='%H:%M:%S')
-    # logging.debug('Debugging message')
-    logging.info('My first step')
-    # logging.warning('And this, too')
-    format_workbook()
+    logging.info('-- Starting --')
+    format_workbook(adjust_content=True, font='Consolas', header=1,
+                    header_bg='0000FF', odd_bg='FF0000',
+                    even_bg='00F700', thousand_separator=True)

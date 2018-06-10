64c6c4ce2b62b0b9c7af3b1f78a1753a8fea01cc
storrellas <storrellas@gmail.com!>
2018-06-10 15:57:15 +0200

    ENH: Evolved version

--- src/email_crawler.py
+++ src/email_crawler.py
@@ -32,8 +32,12 @@ import imaplib
 server_imap = 'imap.gmail.com'
 mail = imaplib.IMAP4_SSL(server_imap)
 mail.login(user, password)
-mail.list()
-mail.select('inbox')
+#mail.list()
+rv, mailboxes = mail.list()
+if rv == 'OK':
+    print("Mailboxes:")
+    print(mailboxes)
+mail.select('INBOX')
 
 type, data = mail.search(None, 'ALL')
 mail_ids = data[0]
@@ -43,16 +47,26 @@ latest_email_id = int(id_list[-1])
 
 print(id_list)
 
-# for i in range(latest_email_id, first_email_id, -1):
-#     typ, data = mail.fetch(i, '(RFC822)')
-#
-#     print(data)
-#     """
-#     for response_part in data:
-#         if isinstance(response_part, tuple):
-#             msg = email.message_from_string(response_part[1])
-#             email_subject = msg['subject']
-#             email_from = msg['from']
-#             print('From : ' + email_from + '\n')
-#             print('Subject : ' + email_subject + '\n')
-#     """
+# rv, data = mail.fetch(str(1), '(RFC822)')
+# if rv != 'OK':
+#     print("ERROR getting message", num)
+
+#for i in range(latest_email_id, first_email_id, -1):
+for i in [5]:
+    typ, data = mail.fetch(str(i), '(RFC822)')
+
+    print("NEEEEEEXXXXXXXXXXXXTTTTTTTTT")
+    print("NEEEEEEXXXXXXXXXXXXTTTTTTTTT")
+    print("NEEEEEEXXXXXXXXXXXXTTTTTTTTT")
+    #print(data)
+    for response_part in data:
+        print("-- SEPARATOR --")
+        print(response_part)
+        """
+        if isinstance(response_part, tuple):
+            msg = email.message_from_string(response_part[1])
+            email_subject = msg['subject']
+            email_from = msg['from']
+            print('From : ' + email_from + '\n')
+            print('Subject : ' + email_subject + '\n')
+        """
